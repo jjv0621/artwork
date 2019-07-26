f109f4ddc22e332fa0b1e8cdd43439d986ca749c
Jessie J Valladares <jessie.j.valldares@gmail.com>
2019-07-26 12:24:51 -0400

    Added apollo client

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+// Import Apollo related modules
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import App from './components/App/App';
+import * as serviceWorker from './serviceWorker';
+import rootReducer from './redux';
+
+const store = createStore(rootReducer);
+
+const cache = new InMemoryCache();
+
+const BASE_URL = 'https://api.artsy.net/api/artworks';
+
+const httpLink = new HttpLink({
+  uri: BASE_URL,
+  headers: {
+    authorization: `Bearer ${
+      process.env.REACT_APP_ACCESS_TOKEN
+    }`,
+  },
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
